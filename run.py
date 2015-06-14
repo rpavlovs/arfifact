dee16f3cf4f1213b665fd629d0c23088265b6c5c
Roman Pavlovskyi <wilohvost@gmail.com>
2015-06-13 21:53:03 -0400

    add recording logic

--- run.py
+++ run.py
@@ -2,26 +2,41 @@ import pyaudio
 import wave
 import sys
 
-CHUNK = 1024
+
+chunk = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 10
+
 
 if len(sys.argv) < 2:
-    print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
+    print("Records a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
     sys.exit(-1)
 
-wf = wave.open(sys.argv[1], 'rb')
-
 p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                output=True,
+                frames_per_buffer=chunk)
 
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
+wf = wave.open(sys.argv[1], 'w')
+wf.setnchannels(1) # mono
+wf.setsampwidth(2) # paInt16 is 2 bytes?
+wf.setframerate(RATE)
 
-data = wf.readframes(CHUNK)
 
-while data != '':
-    stream.write(data)
-    data = wf.readframes(CHUNK)
+print "* recording"
+for i in range(0, RATE / chunk * RECORD_SECONDS):
+    data = stream.read(chunk)
+    wf.writeframes(data)
+
+print "* done"
+
+
+wf.close()
 
 stream.stop_stream()
 stream.close()
